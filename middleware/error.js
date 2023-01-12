e26a678e8507b62ad5e7b496047758f76a22481a
Yuval Levy <lyuval1210@gmail.com>
2023-01-12 14:13:08 +0200

    Updated to version 1.3
    Finished Implementation of Google Calendar.

--- middleware/error.js
+++ middleware/error.js
@@ -1,5 +1,5 @@
 module.exports = function (err, req, res, next) {
   if (err.name === "HttpError")
     return res.status(err.statusCode).send(err.message);
-  else return res.status(500).send("Something Failed!");
+  else return res.status(500).send(`Something Failed: ${err.message}`);
 };
