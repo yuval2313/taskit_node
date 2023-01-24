c3f57cac458915b223f690d45d9809bfd0645761
Yuval Levy <lyuval1210@gmail.com>
2023-01-24 15:09:50 +0200

    Modified environment variables startup checks

--- startup/config.js
+++ startup/config.js
@@ -1,24 +1,46 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const nodeEnv = process.env.NODE_ENV;
+const {
+  NODE_ENV,
+  PORT,
+  JWTKEY,
+  OAUTH_CLIENT_ID,
+  OAUTH_CLIENT_SECRET,
+  OAUTH_REDIRECT_URI,
+} = process.env;
 
-if (!nodeEnv || nodeEnv === "development") {
+if (!JWTKEY)
+  throw new Error("JWTKEY is undefined! Please configure a private key.");
+
+if (!PORT)
+  throw new Error("PORT is undefined! Please configure a port for connection.");
+
+if (!OAUTH_CLIENT_ID)
+  throw new Error(
+    "OAUTH_CLIENT_ID is undefined! Please configure your OAuth client ID."
+  );
+if (!OAUTH_CLIENT_SECRET)
+  throw new Error(
+    "OAUTH_CLIENT_SECRET is undefined! Please configure your OAuth client secret."
+  );
+if (!OAUTH_REDIRECT_URI)
+  throw new Error(
+    "OAUTH_REDIRECT_URI is undefined! Please configure a redirect URI to your frontend application."
+  );
+
+if (!NODE_ENV || NODE_ENV === "development") {
+  const { DB_URL } = process.env;
+  if (!DB_URL)
+    throw new Error(
+      "DB_URL is undefined! Please set your database connection string."
+    );
   console.log(
-    `To enable database replica sets in development (for use with transactions): Please configure custom DBURL with MongoDB connection string.`
+    `To enable database replica sets in development (for use with transactions): Please configure DBURL with MongoDB connection string to a live database.`
   );
 }
-if (nodeEnv === "production") {
-  const {
-    PORT,
-    DB_HOST,
-    DB_USERNAME,
-    DB_PASSWORD,
-    JWTKEY,
-    OAUTH_CLIENT_ID,
-    OAUTH_CLIENT_SECRET,
-    OAUTH_REDIRECT_URI,
-  } = process.env;
+if (NODE_ENV === "production") {
+  const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
 
   if (!DB_HOST)
     throw new Error(
@@ -31,26 +53,5 @@ if (nodeEnv === "production") {
   if (!DB_PASSWORD)
     throw new Error(
       "DB_PASSWORD is undefined! Please set your MongoDB password."
-    );
-
-  if (!JWTKEY)
-    throw new Error("JWTKEY is undefined! Please configure a private key.");
-
-  if (!PORT)
-    throw new Error(
-      "PORT is undefined! Please configure a port for connection."
-    );
-
-  if (!OAUTH_CLIENT_ID)
-    throw new Error(
-      "OAUTH_CLIENT_ID is undefined! Please configure your OAuth client ID."
-    );
-  if (!OAUTH_CLIENT_SECRET)
-    throw new Error(
-      "OAUTH_CLIENT_SECRET is undefined! Please configure your OAuth client secret."
-    );
-  if (!OAUTH_REDIRECT_URI)
-    throw new Error(
-      "OAUTH_REDIRECT_URI is undefined! Please configure a redirect URI to your frontend application."
     );
 }
